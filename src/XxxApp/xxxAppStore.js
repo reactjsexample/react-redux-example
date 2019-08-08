0ee3a94d304dd3c0deef1fc79c4f6fd77550bf7c
ReactJSExample <info@reactjs-example.com>
2019-08-07 21:21:03 -0400

    update store

--- src/XxxApp/xxxAppStore.js
+++ src/XxxApp/xxxAppStore.js
@@ -1,6 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import xxxAppReducer from "./xxxAppReducer";
 
-const xxxAppStore = createStore(xxxAppReducer);
+const xxxAppStore = createStore(
+  xxxAppReducer /* preloadedState, */ + window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default xxxAppStore;
