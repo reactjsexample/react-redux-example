bf65d3c0820ba25adcfc803bfbb69b8a4ce84b8a
ReactJSExample <info@reactjs-example.com>
2019-09-06 07:34:15 -0400

    logo update

--- src/components/XxxHeader/XxxHeader.jsx
+++ src/components/XxxHeader/XxxHeader.jsx
@@ -1,19 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import logo from '../../assets/images/logo.svg';
-import styles from './XxxHeader.module.scss';
-import XxxSearchBox from '../XxxSearchBox/XxxSearchBox';
+import reduxLogoWhite from "../../assets/images/redux-logo-white.svg";
+import styles from "./XxxHeader.module.scss";
+import XxxSearchBox from "../XxxSearchBox/XxxSearchBox";
 
 class XxxHeader extends Component {
   render() {
     return (
-        <div className={styles.header}>
-          <img src={logo} className={styles.logo} alt="logo"/>
-          <div className={styles.headerTitle}>React Redux Example</div>
-          <div className={styles.searchBoxContainer}>
-            <XxxSearchBox/>
-          </div>
+      <div className={styles.header}>
+        <img src={reduxLogoWhite} className={styles.logo} alt="logo" />
+        <div className={styles.headerTitle}>React Redux Example</div>
+        <div className={styles.searchBoxContainer}>
+          <XxxSearchBox />
         </div>
+      </div>
     );
   }
 }
